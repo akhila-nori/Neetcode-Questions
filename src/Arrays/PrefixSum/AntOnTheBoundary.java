6a1e09e60dcebe8ff86990c266e80c771695b7e3
akhila-nori <akhilanori2001@gmail.com>
2026-03-21 14:10:22 +0530

    Prefix sum - Ant on the boundary

---
+++ src/Arrays/PrefixSum/AntOnTheBoundary.java
@@ -0,0 +1,15 @@
+package Arrays.PrefixSum;
+
+class AntOnTheBoundary {
+    public int returnToBoundaryCount(int[] nums) {
+        int sum = 0, count = 0;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum == 0){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
