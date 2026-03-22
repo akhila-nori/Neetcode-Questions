6414f088fd9dcc808860ce4b6432632cefbf9579
akhila-nori <akhilanori2001@gmail.com>
2026-03-22 09:40:09 +0530

    Brute Force - Sub array sum equals k comments

--- src/Arrays/PrefixSum/SubArraySumEqualskBruteForce.java
+++ src/Arrays/PrefixSum/SubArraySumEqualskBruteForce.java
@@ -1,5 +1,6 @@
 package Arrays.PrefixSum;
 
+//O(n^2) time complexity - you are trying to compute contiguous subarrays again and again
 class SubArraySumEqualskBruteForce {
     public int subarraySum(int[] nums, int k) {
         int sumInner, c=0;
