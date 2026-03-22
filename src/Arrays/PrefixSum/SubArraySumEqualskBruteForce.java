ed8ecc3153ea5266693973df25105038ac4a8567
akhila-nori <akhilanori2001@gmail.com>
2026-03-22 09:36:57 +0530

    Brute Force - Sub array sum equals k

---
+++ src/Arrays/PrefixSum/SubArraySumEqualskBruteForce.java
@@ -0,0 +1,17 @@
+package Arrays.PrefixSum;
+
+class SubArraySumEqualskBruteForce {
+    public int subarraySum(int[] nums, int k) {
+        int sumInner, c=0;
+        for(int i=0;i<nums.length;i++){
+            sumInner=0;
+            for(int j=i;j<nums.length;j++){
+                sumInner += nums[j];
+                if(sumInner==k) c++;
+            }
+
+        }
+        return c;
+
+    }
+}
