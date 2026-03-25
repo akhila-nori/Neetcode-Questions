c82d9b2612c407c66e714f5bb020a1c5e225c89a
akhila-nori <akhilanori2001@gmail.com>
2026-03-25 21:54:11 +0530

    Brute force - contiguous array

---
+++ src/Arrays/HashMap/ContiguousArray.java
@@ -0,0 +1,25 @@
+package Arrays.HashMap;
+
+class ContiguousArray {
+    public int findMaxLength(int[] nums) {
+
+        int maxLen = 0;
+        for(int i=0;i<nums.length;i++){
+            int zeros=0;
+            int ones=0;
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]==0) {
+                    zeros++;
+                } else ones++;
+
+                if(zeros == ones){
+                    maxLen = Math.max(maxLen,j-i+1);
+                }
+
+            }
+
+        }
+        return maxLen;
+
+    }
+}
